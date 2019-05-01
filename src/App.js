f89f1783ad63daa185acf902fa58970f73f4a186
Samson O'Shaughnessy <soshaughnessy300@gmail.com>
2019-05-01 16:40:04 -0400

    bind demo

--- src/App.js
+++ src/App.js
@@ -8,9 +8,35 @@ import Cats from './Cats';
 
 import {Link, Route} from 'react-router-dom';
 
-function App() {
+
+
+
+
+
+
+
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0
+    };
+    this._incrementCount = this._incrementCount.bind(this);
+  }
+
+  componentDidMount() {
+    // this._incrementCount.call(this, 5);
+    // this._incrementCount.apply(this, [5]);
+  } 
+
+  render() {
   return (
     <div className="App">
+      <h1>{this.state.counter}</h1>
+      <button onClick={this._incrementCountUsingArrow}>increment</button>
+
       <Link to="/">Home</Link>
       <Link to="/cats">Cats</Link>
       <Link to="/about">About</Link>
@@ -23,6 +49,20 @@ function App() {
       <Route path="/about" component={About}/>
     </div>
   );
+}
+  // gives error without bind
+  _incrementCount(howMuchBy=1) {
+    this.setState({
+      counter: this.state.counter + howMuchBy
+    });
+  }
+
+  // Arrow functions apply .bind
+  _incrementCountUsingArrow = () => {
+    this.setState({
+      counter:this.state.counter + 1
+    })
+  }
 }
 
 export default App;
